d033afdd83b464c46604d7213e19d8a9b6df6017
Alfonso Lluch <alfonso.lluch@kaizenreporting.com>
2023-06-13 19:09:11 +0200

    removing commented code from firebase

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -46,12 +46,3 @@ exports.userCreated = functions.auth.user().onCreate((user) => {
         });
     });
 });
-
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
